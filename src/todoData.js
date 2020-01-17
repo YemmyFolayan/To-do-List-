d2e258df78e69b9a24929039103076cbcc2add73
Amarachi Amaechi <amarachiamaechi4@gmail.com>
2020-01-17 10:09:29 +0100

    All changes added

---
+++ src/todoData.js
@@ -0,0 +1,57 @@
+
+
+const todoData = [
+
+    {
+        id: 1,
+        text: " Pray for 5 minutes",
+        completed: false
+
+    },
+
+    
+    {
+        id: 2,
+        text: "Brush my teeth ",
+        completed: false
+
+    },
+
+
+
+    
+    {
+        id: 3,
+        text: " Take my bath",
+        completed: false
+
+    },
+
+    
+    {
+        id: 4,
+        text: " Read a book ",
+        completed: false
+ 
+    },
+
+
+
+    
+    {
+        id: 5,
+        text: "Research about Robots",
+        completed: false
+
+    }
+
+   
+
+
+
+
+
+
+
+]
+ export default todoData
